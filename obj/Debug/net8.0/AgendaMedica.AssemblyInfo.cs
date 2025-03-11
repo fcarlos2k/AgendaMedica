38cbc5ed0913c2af98c218e5e6adba03253342f9
Fabrício Carlos Araújo da Silva <fabricio.araujo@cg.df.gov.br>
2025-03-11 11:40:46 -0300

    Teste de commit

--- obj/Debug/net8.0/AgendaMedica.AssemblyInfo.cs
+++ obj/Debug/net8.0/AgendaMedica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgendaMedica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5847ccb859bc85e3f06c19b101cec07a70c672bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+587865a9d10fceda948f496c65cef9b1afcf083b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgendaMedica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgendaMedica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
